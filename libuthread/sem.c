738dfa0146e79599ca6aef730f382dd047e88447
mhhuang95 <mhhuang@ucdavis.edu>
2019-05-05 13:17:42 -0700

    phase1

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -29,58 +29,60 @@ sem_t sem_create(size_t count)
 int sem_destroy(sem_t sem)
 {
 	/* TODO Phase 1 */
+    enter_critical_section();
     if (sem == NULL){
+        exit_critical_section();
         return -1;
     }
     if (queue_destroy(sem->block_queue) == -1){
+        exit_critical_section();
         return -1;
     }
     free(sem);
+    exit_critical_section();
     return 0;
 }
 
 int sem_down(sem_t sem)
 {
 	/* TODO Phase 1 */
+    enter_critical_section();
     if (sem == NULL){
+        exit_critical_section();
         return -1;
     }
-    enter_critical_section();
+    
     if (sem->curr_count != 0){
         sem->curr_count--;
-        exit_critical_section();
-    }
-    else{
-        exit_critical_section();
+    }else{
+        
         //free tid when dequeue;
         pthread_t* tid;
         tid = malloc(sizeof(pthread_t));
         *tid = pthread_self();
-        enter_critical_section();
+        
         queue_enqueue(sem->block_queue, tid);
-        exit_critical_section();
+        
         thread_block();
-        //trying to take resource
-        sem_down(sem);
     }
+    exit_critical_section();
     return 0;
 }
 
 int sem_up(sem_t sem)
 {
 	/* TODO Phase 1 */
+    enter_critical_section();
     if (sem == NULL){
+        exit_critical_section();
         return -1;
     }
     
-    enter_critical_section();
-    sem->curr_count ++;
-    exit_critical_section();
-    
     pthread_t* tid;
-    enter_critical_section();
     if (queue_dequeue(sem->block_queue,(void **) &tid) == 0){
         thread_unblock(*tid);
+    }else {
+        sem->curr_count ++;
     }
     exit_critical_section();
     return 0;
@@ -90,16 +92,17 @@ int sem_up(sem_t sem)
 int sem_getvalue(sem_t sem, int *sval)
 {
 	/* TODO Phase 1 */
+    enter_critical_section();
     if (sem == NULL || sval == NULL){
+        exit_critical_section();
         return -1;
     }
     if (sem->curr_count > 0){
         *sval = sem->curr_count;
     }else{
-        enter_critical_section();
         *sval = -1* queue_length(sem->block_queue);
-        exit_critical_section();
     }
+    exit_critical_section();
     return 0;
 }
 
