63f2a31165c2802d0a22c33133351b4e580525cf
Theacalix <Theacalix@users.noreply.github.com>
2019-05-12 16:16:33 -0700

    refactored code: comments spacing

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -12,7 +12,6 @@
 #include "thread.h"
 #include "tps.h"
 
-/* TODO: Phase 2 */
 typedef struct page{
     int reference_count;
     void* addr;
@@ -27,7 +26,7 @@ queue_t tps_queue;
 
 static int find_tps(void* data, void* argv){
     pthread_t tid = *((pthread_t*)argv);
-    
+
     if(((tps*)data)->tid == tid){
         return 1;
     }
@@ -36,7 +35,7 @@ static int find_tps(void* data, void* argv){
 
 static int find_addr(void* data, void* argv){
     void* addr = argv;
-    
+
     if(((tps*)data)->tps_page->addr == addr){
         return 1;
     }
@@ -51,17 +50,17 @@ static void segv_handler(int sig, siginfo_t *si, void *context)
      * fault occurred
      */
     void *p_fault = (void*)((uintptr_t)si->si_addr & ~(TPS_SIZE - 1));
-    
+
     /*
      * Iterate through all the TPS areas and find if p_fault matches one of them
      */
-    
+
     queue_iterate(tps_queue, find_addr, p_fault, (void**)&my_tps);
-    
+
     if (my_tps != NULL)
     /* Printf the following error message */
         fprintf(stderr, "TPS protection error!\n");
-    
+
     /* In any case, restore the default signal handlers */
     signal(SIGSEGV, SIG_DFL);
     signal(SIGBUS, SIG_DFL);
@@ -74,7 +73,7 @@ static void segv_handler(int sig, siginfo_t *si, void *context)
 int tps_init(int segv)
 {
 	/* TODO: Phase 2 */
-    
+
     if (tps_queue != NULL){
         return -1;
     }
@@ -82,18 +81,18 @@ int tps_init(int segv)
     if (tps_queue == NULL){
         return -1;
     }
-    
+
     if (segv) {
         struct sigaction sa;
-        
+
         sigemptyset(&sa.sa_mask);
         sa.sa_flags = SA_SIGINFO;
         sa.sa_sigaction = segv_handler;
         sigaction(SIGBUS, &sa, NULL);
         sigaction(SIGSEGV, &sa, NULL);
     }
-    
-    
+
+
     return 0;
 }
 
@@ -108,28 +107,28 @@ int tps_create(void)
     if (my_tps != NULL){
         return -1;
     }
-    
+
     my_tps = malloc(sizeof(tps));
     if (my_tps == NULL){
         return -1;
     }
-    
+
     my_tps->tps_page = malloc(sizeof(page));
     if (my_tps->tps_page == NULL){
         return -1;
     }
-    
+
     my_tps->tps_page->addr = mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANON, 10, 0);
     if (my_tps->tps_page->addr == MAP_FAILED){
         return -1;
     }
-    
+
     my_tps->tid = cur_tid;
     my_tps->tps_page->reference_count = 1;
     enter_critical_section();
     queue_enqueue(tps_queue, (void*)my_tps);
     exit_critical_section();
-    
+
     return 0;
 }
 
@@ -150,8 +149,8 @@ int tps_destroy(void)
     munmap(my_tps->tps_page->addr, TPS_SIZE);
     free(my_tps->tps_page);
     free(my_tps);
-    
-    
+
+
     return 0;
 }
 
@@ -191,7 +190,7 @@ int tps_write(size_t offset, size_t length, char *buffer)
             return -1;
         }
         new_page->reference_count = 1;
-        
+
         mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_READ);
         memcpy(new_page->addr, my_tps->tps_page->addr, TPS_SIZE);
         mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_NONE);
@@ -200,7 +199,7 @@ int tps_write(size_t offset, size_t length, char *buffer)
         my_tps->tps_page = new_page;
     }
     exit_critical_section();
-    
+
     mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_WRITE);
     memcpy( my_tps->tps_page->addr + offset, (void*)buffer, length);
     mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_NONE);
@@ -217,25 +216,25 @@ int tps_clone(pthread_t tid)
     if (target_tps == NULL){
         return -1;
     }
-    
-    
+
+
     /*
     if (tps_create() == -1){
         return -1;
     }
-    
+
     tps* my_tps;
     pthread_t cur_tid = pthread_self();
     enter_critical_section();
     queue_iterate(tps_queue, find_tps, (void*)&cur_tid, (void**)&my_tps);
     exit_critical_section();
-    
+
     mprotect(target_tps->tps_page->addr, TPS_SIZE, PROT_READ);
     mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_WRITE);
     memcpy(my_tps->tps_page->addr, target_tps->tps_page->addr, TPS_SIZE);
     mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_NONE);
     mprotect(target_tps->tps_page->addr, TPS_SIZE, PROT_NONE);
-     
+
     */
     pthread_t cur_tid = pthread_self();
     tps* my_tps;
@@ -245,7 +244,7 @@ int tps_clone(pthread_t tid)
     if (my_tps != NULL){
         return -1;
     }
-    
+
     my_tps = malloc(sizeof(tps));
     if (my_tps == NULL){
         return -1;
@@ -253,11 +252,10 @@ int tps_clone(pthread_t tid)
     enter_critical_section();
     my_tps->tps_page = target_tps->tps_page;
     my_tps->tps_page->reference_count ++;
-    
+
     my_tps->tid = cur_tid;
     queue_enqueue(tps_queue, (void*)my_tps);
     exit_critical_section();
-    
+
     return 0;
 }
-
--- test/tps_err.c
+++ test/tps_err.c
@@ -12,129 +12,148 @@
 static sem_t sem3, sem4;
 
 void *latest_mmap_addr; // global variable to make address returned by mmap accessible
+
+//wrapper to save location of page
 void *__real_mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off);
-void *__wrap_mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off){
-    latest_mmap_addr = __real_mmap(addr, len, prot, flags, fildes, off);
-    return latest_mmap_addr;
+void *__wrap_mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
+{
+  latest_mmap_addr = __real_mmap(addr, len, prot, flags, fildes, off);
+  return latest_mmap_addr;
 }
 
-
+//gets page and tries to access it illegally, causing a seg fault
 void *thread1(void* arg)
 {
+	tps_create();
 
-	assert(tps_create() == 0);
-    assert(tps_create() == -1);
+  //seg fault
+  char *tps_addr = latest_mmap_addr;
 
-    /*seg fault*/
-    char *tps_addr = latest_mmap_addr;
-    
-    tps_addr[0] = '\0';
-    
-	assert(tps_destroy() == 0);
-    assert(tps_destroy() == -1);
-    
+  tps_addr[0] = '\0';
+
+  tps_destroy();
+
 	return NULL;
 }
 
-void *thread2(void* arg){
-    char *buffer = malloc(TPS_SIZE);
-    memset(buffer, 0, TPS_SIZE);
-    
-    assert(tps_read(0, TPS_SIZE, buffer) == -1);
-    
-    tps_create();
-    
-    assert(tps_read(0, TPS_SIZE + 1, buffer) == -1);
-    
-    assert(tps_read(1, TPS_SIZE, buffer) == -1);
-    
-    assert(tps_read(0, TPS_SIZE, NULL) == -1);
-    
-    tps_destroy();
-    
-    assert(tps_write(0, TPS_SIZE, buffer) == -1);
-    
-    tps_create();
-    
-    assert(tps_write(0, TPS_SIZE + 1, buffer) == -1);
-    
-    assert(tps_write(1, TPS_SIZE, buffer) == -1);
-    
-    assert(tps_write(0, TPS_SIZE, NULL) == -1);
-    
-    tps_destroy();
-    
-    return NULL;
+//checks create, destroy, read, and write error cases
+void *thread2(void* arg)
+{
+  char *buffer = malloc(TPS_SIZE);
+  memset(buffer, 0, TPS_SIZE);
+
+  //read with no tps fails
+  assert(tps_read(0, TPS_SIZE, buffer) == -1);
+
+  assert(tps_create() == 0);
+
+  //create twice fails
+  assert(tps_create() == -1);
+
+  //access out of bounds fails: length
+  assert(tps_read(0, TPS_SIZE + 1, buffer) == -1);
+
+  //access out of bounds fails: offset
+  assert(tps_read(1, TPS_SIZE, buffer) == -1);
+
+  //null buffer fails
+  assert(tps_read(0, TPS_SIZE, NULL) == -1);
+
+  tps_destroy();
+
+  //write no tps fails
+  assert(tps_write(0, TPS_SIZE, buffer) == -1);
+
+  tps_create();
+
+  //write out of bound length fails
+  assert(tps_write(0, TPS_SIZE + 1, buffer) == -1);
+
+  //write out of bound offset fails
+  assert(tps_write(1, TPS_SIZE, buffer) == -1);
+
+  //write null buffer fails
+  assert(tps_write(0, TPS_SIZE, NULL) == -1);
+
+  assert(tps_destroy() == 0);
+  //destroy twice fails
+  assert(tps_destroy() == -1);
+
+  return NULL;
 }
 
 void *thread4(void* arg)
 {
-    tps_create();
-    sem_up(sem3);
-    sem_down(sem4);
-    
-    return NULL;
+  //create tps for testing later
+  tps_create();
+  sem_up(sem3);
+  sem_down(sem4);
+
+  return NULL;
 }
 
 
 void *thread3(void* arg)
 {
-    pthread_t tid;
-    pthread_create(&tid, NULL, thread4, NULL);
-    
-    assert(tps_clone(tid) == -1);
-    
-    tps_create();
-    
-    sem_down(sem3);
-    
-    assert(tps_clone(tid) == -1);
-    
-    sem_up(sem4);
-    
-    return NULL;
+  pthread_t tid;
+  //create thread 4
+  pthread_create(&tid, NULL, thread4, NULL);
+
+  //clone a tps that doesnt exist fails
+  assert(tps_clone(tid) == -1);
+
+  tps_create();
+  sem_down(sem3);
+
+  //clone when we already have a tps fails
+  assert(tps_clone(tid) == -1);
+
+  sem_up(sem4);
+
+  return NULL;
 }
 
-void test1(){
-    pthread_t tid;
-    
-    /* Create thread 1 and wait */
-    pthread_create(&tid, NULL, thread1, NULL);
-    pthread_join(tid, NULL);
+void test_seg()
+{
+  pthread_t tid;
+
+  //create thread 1 and wait
+  pthread_create(&tid, NULL, thread1, NULL);
+  pthread_join(tid, NULL);
 }
 
-void test2(){
-    pthread_t tid;
-    
-    pthread_create(&tid, NULL, thread2, NULL);
-    pthread_join(tid, NULL);
-    
+void test_read_write_create_destroy(){
+  pthread_t tid;
+
+  pthread_create(&tid, NULL, thread2, NULL);
+  pthread_join(tid, NULL);
+
 }
 
-void test3(){
-    pthread_t tid;
-    
-    sem3 = sem_create(0);
-    sem4 = sem_create(0);
-    
-    pthread_create(&tid, NULL, thread3, NULL);
-    pthread_join(tid, NULL);
-    
-    sem_destroy(sem3);
-    sem_destroy(sem4);
-    
+void test_clone()
+{
+  pthread_t tid;
+
+  //create sems for syncing
+  sem3 = sem_create(0);
+  sem4 = sem_create(0);
+
+  //create and wait
+  pthread_create(&tid, NULL, thread3, NULL);
+  pthread_join(tid, NULL);
+
+  sem_destroy(sem3);
+  sem_destroy(sem4);
 }
 
 int main(int argc, char **argv)
 {
-    assert(tps_init(1) == 0);
-    assert(tps_init(1) == -1);
-    
-    test2();
-    test3();
-    test1();
-    
+  assert(tps_init(1) == 0);
+  assert(tps_init(1) == -1);
+
+  test_read_write_create_destroy();
+  test_clone();
+  test_seg();
+
 	return 0;
 }
-
-
